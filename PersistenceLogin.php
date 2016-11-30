ec2b801f726d19a7cdb9fff5790f8fb1005df802
Anchal <Anchalvishwakarma@users.noreply.github.com>
2016-11-30 13:18:02 +0530

    Remember me class

---
+++ PersistenceLogin.php
@@ -0,0 +1,282 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * This class will manage persistence user Login.
+ * user will logged in for a single day if it's not logout.
+ * a remember me checkbox is given to enable persistence login at time of login.
+ * 
+ *  */
+
+class PersistenceLogin {
+    /*
+     *  private variable hold CI Instance
+     */
+
+    private $CI;
+
+    /*
+     *  private variable for User data
+     */
+    private $userData = array();
+
+    /*
+     *  private variable hash value
+     */
+    private $hash = 'gdfeahahSHHW766JHKSKGSJAGDJASGDFDdsds';
+
+    /*
+     * Cookie private variable
+     * comes from browser
+     */
+    private $cookieValue;
+
+
+    /*
+     * username private variable
+     */
+    private $identifier;
+
+
+    /*
+     * username private variable
+     */
+    private $token;
+
+    /*
+     * username private variable
+     * come from browser
+     */
+    private $cookie_username;
+
+
+    /*
+     * identifier private variable
+     * come from browser
+     */
+    private $cookie_identifier;
+
+
+    /*
+     * token private variable
+     * come from browser
+     */
+    private $cookie_token;
+
+    /*
+     * All userInput Data from login form
+     * come from browser
+     */
+    private $input_array = array();
+
+    /*
+     * check user with database exist or not
+     * */
+    private $is_user_exist = FALSE;
+
+    public function __construct() {
+        $this->CI = & get_instance();
+        $this->CI->load->model('Users_model');
+        $this->CI->load->model('bu_model');
+        $this->CI->load->helper('cookie');
+        $this->CI->load->library('encrypt');
+        $this->input_array = $this->CI->input->post();
+    }
+
+    /*
+     * set Cookie for one day
+     * use class variable for operation
+     * @classvariable :  $this->input_array
+     * @classvariable :  $this->identifier
+     * @classvariable :  $this->token
+     * */
+
+    public function CustomSetCookie() {
+        $cookieData = array(
+            'name' => 'user',
+            'value' => $this->CI->encrypt->encode($this->input_array['user_name'] . "-" . $this->identifier . "-" . $this->token),
+            'expire' => strtotime('tomorrow') - time() //set seconds for next day left
+        );
+        set_cookie($cookieData);
+    }
+
+    /*
+     * Get Cookie forom browser
+     * set class variable :  $this->cookieValue
+     * */
+
+    public function CustomGetCookie() {
+        $this->cookieValue = ($this->CI->input->cookie('user', TRUE) != NULL) ? $this->CI->input->cookie('user', TRUE) : NULL;
+    }
+
+    /*
+     * check user exist or not cross check with DB
+     * use class variable for operation
+     * @class variable :  $this->input_array
+     * @class variable :  $this->is_user_exist
+     * */
+
+    public function isUserExist() {
+        $this->CI->db->select('*');
+        $this->CI->db->from('users');
+        $this->CI->db->where('users.user_name', $this->input_array['user_name']);
+        $query = $this->CI->db->get();
+        $this->is_user_exist = ($query->num_rows() == 1) ? TRUE : FALSE;
+    }
+
+    /*
+     * check user come with rememberme option
+     * use class variable for operation
+     * @class variable :  $this->input_array
+     * @class variable :  $this->identifier
+     * @class variable :  $this->token
+     * */
+
+    public function isRememberMe() {
+        if (isset($this->input_array['rememberme']) && $this->input_array['rememberme'] == 1) {
+            $this->isUserExist();
+            $this->generateTokenAndIdentifires();
+
+            //save token and identifier in users table for log-in user and set cookie
+            if ($this->is_user_exist === TRUE) {
+                $this->CustomSetCookie();
+                $userData = array(
+                    'identifier' => $this->identifier,
+                    'token' => $this->token,
+                );
+
+                $this->CI->db->where('user_name', $this->input_array['user_name']);
+                $this->CI->db->update('users', $userData);
+            }
+        } else {
+            return FALSE;
+        }
+    }
+
+    /*
+     * create hashs for cookie
+     * use class variable for operation
+     * @class variable :  $this->input_array
+     * @class variable :  $this->identifier
+     * @class variable :  $this->token
+     * */
+
+    public function generateTokenAndIdentifires() {
+        $this->identifier = md5($this->input_array['user_name']);
+        $this->token = hash('sha512', $this->hash);
+    }
+
+    /*
+     * Delete cookie when user after clicking on logout
+     * use CI session userData B,coz user_name field will present at session
+     * @session variable :  session->userdata('user_name')
+     * */
+
+    public function deleteCookie() {
+        delete_cookie('user');
+        //set NUll to user token and identifier
+        $userData = array(
+            'identifier' => NULL,
+            'token' => NULL,
+        );
+
+        $this->CI->db->where('user_name', $this->CI->session->userdata('user_name'));
+        $this->CI->db->update('users', $userData);
+    }
+
+    /*
+     * check user for Persistence Cookie based Login
+     * @class variable :  $this->cookieValue
+     * @case1: if cookie not found than redirect to login page
+     * @case2: if user has valid cookie than set userdata and log-in
+     * @case3: if user has invalid cookie than redirect to login page
+     */
+
+    public function checkForPersistanceLogin() {
+        $this->CustomGetCookie();
+        //if cookie value not set perform valid cookie check
+        if ($this->cookieValue != NULL) {
+            $this->decodeAndSetCookieValue();
+            $this->isCookieUserValid();
+        }
+    }
+
+    /*
+     * decode cookie data and assign to instance variable
+     * use class variable for operation
+     * @class variable :  $this->cookieValue
+     * @class variable :  $this->cookie_username
+     * @class variable :  $this->cookie_identifier
+     * @class variable :  $this->token
+     * */
+
+    public function decodeAndSetCookieValue() {
+        $cookieArray = explode('-', $this->CI->encrypt->decode($this->cookieValue));
+        $this->cookie_username = $cookieArray[0];
+        $this->cookie_identifier = $cookieArray[1];
+        $this->cookie_token = $cookieArray[2];
+    }
+
+    /*
+     * check function for user if data comes from cookie
+     * if user valid than redirect  to dashboard
+     * use class variable for operation
+     * @class variable :  $this->cookie_identifier
+     * @class variable :  $this->token
+     * */
+
+    public function isCookieUserValid() {
+        $this->CI->db->select('*');
+        $this->CI->db->from('users');
+        $this->CI->db->where('users.identifier', $this->cookie_identifier);
+        $this->CI->db->where('users.token', $this->cookie_token);
+        $query = $this->CI->db->get();
+        $this->userData = $query->result_array();
+        $rowCount = $query->num_rows();
+
+        if ($query->num_rows() == 1 && $this->cookie_identifier == $this->userData[0]['identifier'] && $this->cookie_token == $this->userData[0]['token']) {
+            $this->setUserDataToSession();
+            //valid user case redirect to dashboard
+            redirect('dashboard');
+            die;
+        }
+    }
+
+    /*
+     * set user data to session when user persistence cookie is valid
+     * use class variable for operation
+     * @class variable :  $this->userData
+     * */
+
+    public function setUserDataToSession() {
+        $user_details = $this->userData[0];
+        $other_bu_project = NULL;
+        $user_rights = $this->CI->Users_model->get_user_rights($user_details['ID']);
+
+        if ($user_details['ID'] != 1) {
+            //get user BU Detail
+            $bu_info = $this->CI->bu_model->get_user_bu($user_details['ID']);
+            $bu_id = $bu_info['bu_id'];
+            $bu_category = $bu_info['bu_category'];
+            //get sharable project's detail
+            $other_bu_project = $this->CI->Users_model->get_other_bu_project_as_assing_sharable($user_details['ID'], $bu_id);
+        } else {
+            $bu_id = -1;
+        }
+        $data = array(
+            'user_name' => $user_details['user_name'],
+            'user_id' => $user_details['ID'],
+            'user_fname' => $user_details['first_name'],
+            'is_logged_in' => true,
+            'bu_id' => $bu_id,
+            'user_role' => $user_details['type'],
+            'rights' => $user_rights,
+            'sharable_project_data' => $other_bu_project,
+            'bu_category' => $bu_category
+        );
+
+        $this->CI->session->set_userdata($data);
+    }
+
+}
